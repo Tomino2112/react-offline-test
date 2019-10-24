90658104a511c4bd5179250854896b219522dee8
Tomas <dostal.it@gmail.com>
2019-10-24 17:18:13 +0100

    [API] Sketching tests for helper hook loading generations

---
+++ src/hooks/useLoadGeneration.spec.js
@@ -0,0 +1,5 @@
+describe("useLoadGeneration", () => {
+    test("should return correct state while loading", () => {});
+    test("should return correct state when done", () => {});
+    test("should return correct state when error", () => {});
+});

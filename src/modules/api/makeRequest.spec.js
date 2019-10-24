2a90251efb686c1a516dcaa3d7827b86819cf93e
Tomas <dostal.it@gmail.com>
2019-10-24 12:35:18 +0100

    [API] Sketch out tests - eg. hybrid TDD approach

---
+++ src/modules/api/makeRequest.spec.js
@@ -0,0 +1,12 @@
+describe("makeRequest", () => {
+    describe("success", () => {
+        test("should resolve with response body", async () => {});
+        test("should resolve with empty object", async () => {});
+    });
+
+    describe("failure", () => {
+        test("should reject with status code and error from api", async () => {});
+        test("should reject with status codewhen no response body", async() => {});
+        test("should reject with generic error when request failed", async () => {});
+    });
+});

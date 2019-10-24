d1db3d935bf06a5f841dd8f47c64775abaadc1eb
Tomas <dostal.it@gmail.com>
2019-10-24 23:49:51 +0100

    [UI] Adds icons and some styles to fuels

--- src/components/Fuel/Fuel.js
+++ src/components/Fuel/Fuel.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "../../styles/icons.css";
+import "./style.css";
+import { ucFirst } from "../../utils";
 
 const Fuel = ({ name, percentage }) => {
-    return <>
-        <h3>{name}</h3>
-        <h4>{percentage}%</h4>
-    </>
+    return <div className="fuel">   
+        <div className={`icon icon-${name}`}></div>
+        <h4>{percentage}</h4>
+        <h3>{ucFirst(name)}</h3>
+    </div>
 };
 
 export default Fuel;
